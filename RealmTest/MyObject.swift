d56049c38feb64bf6c14069b22d7606427027655
Suleyman Calik <suleymancalik@gmail.com>
2015-10-21 10:41:58 +0300

    Initial commit

---
+++ RealmTest/MyObject.swift
@@ -0,0 +1,14 @@
+//
+//  MyObject.swift
+//  RealmTest
+//
+//  Created by Suleyman Calik on 21.10.2015.
+//  Copyright © 2015 Suleyman Calik. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class MyObject: Object {
+
+}
---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by Suleyman Calik on 21.10.2015.
+//  Copyright © 2015 Suleyman Calik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myObjectDict:[MyObject:Int] = []
+        
+    }
+
+
+
+}
+
